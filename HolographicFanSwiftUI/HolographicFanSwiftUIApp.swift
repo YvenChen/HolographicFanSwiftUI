1a692426ddbb60669e52caca22dcc2ccf771a244
YvenChen <yvenchen@icloud.com>
2023-04-06 19:38:31 +0800

    Initial Commit

---
+++ HolographicFanSwiftUI/HolographicFanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolographicFanSwiftUIApp.swift
+//  HolographicFanSwiftUI
+//
+//  Created by Yven Chen on 2023/4/6.
+//
+
+import SwiftUI
+
+@main
+struct HolographicFanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
